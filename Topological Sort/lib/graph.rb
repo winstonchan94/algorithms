612b78a73e62b3f4aaa4bb4d59bc55c42184e2f8
Winston Chan <winston.chan94@gmail.com>
2018-05-08 10:09:10 -0700

    Topological Sort

---
+++ Topological Sort/lib/graph.rb
@@ -0,0 +1,29 @@
+class Vertex
+  attr_reader :value, :in_edges, :out_edges
+  def initialize(value)
+    @value = value
+    @in_edges = []
+    @out_edges = []
+  end
+
+  def in_count
+    @in_edges.count
+  end
+end
+
+class Edge
+  attr_reader :from_vertex, :to_vertex, :cost
+  def initialize(from_vertex, to_vertex, cost = 1)
+    @from_vertex = from_vertex
+    @to_vertex = to_vertex
+    @cost = cost
+    to_vertex.in_edges << self
+    from_vertex.out_edges << self
+  end
+
+  def destroy!
+    @to_vertex.in_edges.delete(self)
+    @from_vertex.out_edges.delete(self)
+    @from_vertex, @to_vertex = nil, nil
+  end
+end
---
+++ Topological Sort/lib/topological_sort.rb
@@ -0,0 +1,54 @@
+require_relative 'graph'
+
+# Implementing topological sort using both Khan's and Tarian's algorithms
+
+# Khan's
+def topological_sort(vertices)
+  in_edge_counts = {}
+  no_ins = []
+
+  vertices.each do |vertex|
+    in_edge_counts[vertex] = vertex.in_edges.length
+    no_ins << vertex if vertex.in_edges.empty?
+  end
+
+  result = []
+  while !no_ins.empty?
+    u = no_ins.shift
+    result << u
+
+    u.out_edges.each do |edge|
+      to_vertex = edge.to_vertex
+      in_edge_counts[to_vertex] -= 1
+      no_ins << to_vertex if in_edge_counts[to_vertex] == 0
+    end
+  end
+  if result.length != vertices.length
+    return []
+  end
+  result
+end
+
+# Tarjan's
+def topological_sort_t(vertices)
+  result = []
+  visited = Set.new
+  vertices.each do |vertex|
+    if !visited.include?(vertex)
+      dfs_visit(vertex, visited, result)
+    end
+  end
+  result
+end
+
+def dfs_visit(vertex, visited, result)
+  visited << vertex
+  vertex.out_edges.each do |edge|
+    to_vertex = edge.to_vertex
+    if !visited.include?(to_vertex)
+      dfs_visit(to_vertex, visited, result)
+    end
+  end
+
+  result.unshift(vertex)
+end
